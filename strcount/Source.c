9254b381155df25f8602bbaebc83151d889c98ad
ITFS777 <1374924410@qq.com>
2019-11-14 14:21:19 +0800

    prove strcount

--- strcount/Source.c
+++ strcount/Source.c
@@ -1,30 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
 //////////////////////////////////////////////////////////////////////////
-#define SIZE 128
+
 //////////////////////////////////////////////////////////////////////////
 int main(void)
 {
-	char name[SIZE] = { '\0' };
-	int i = 0, count = 0;
-	while (1)
-	{
-		count = 0;
-		memset(name, '\0', SIZE * sizeof(char));
-		fgets(name, SIZE, stdin);
-		for (i = 0; i < strlen(name);i++)
-		{
-			if (name[i] == '\n')
-				break;
-			if (name[i] != ' ')
-			{
-				count++;
-				for (/*void*/; name[i] != ' '; i++)
-					;
-				i--;
-			}
-		}
-		printf("%d\n", count);
-	}
+    char ch=0;
+    int count=0;
+    while((ch=getchar()) !='\n')
+        if(ch==' ')
+            count++;
+    printf("%d",count+1);
+    return 0;
 }
