2f548eb085caa7db4fd6fd40f563407d3a4d86e3
ITFS777 <1374924410@qq.com>
2020-01-21 16:51:28 +0800

    2020/01/21 16:51:28 by ITFS<Aryan.ITFS@Gmail.com>

--- Python/Base64En&De.py
+++ Python/Base64En&De.py
@@ -1,19 +1,22 @@
 import base64
+newTable = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+oldTable = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
 while True:
-    flag = input('en/de\n')
-    if flag == 'en':
-        string = input()
-        print(str(base64.b64encode(string.encode('utf-8')), 'utf-8'))
+    flag = input('encode[e]/decode[d]/changeTable[c]\n>')
+    if flag == 'e':
+        string = input("string:")
+        print(str(base64.b64encode(string.encode('utf-8'))))
         print()
+    elif flag == 'd':
+        string = input("Base64code:")
+        print(base64.b64decode(string.translate(str.maketrans(newTable,oldTable))))
+        print()
+    elif flag == 'c':
+        print("oldTable:",oldTable,sep='')
+        newTable = input("newTable:")
+        print("Table Changed")
     else:
-        string = input()
-        decode = base64.b64decode(string.encode('utf-8'))
-        print(decode)
-        for x in decode:
-            print(chr(x),end='')
-        print()
-        seq = ''
-        for x in decode:
-             seq += bin(x).replace('b','')
-        print(seq)
-        print()
+        print("Illegal Input!")
+
+
+
