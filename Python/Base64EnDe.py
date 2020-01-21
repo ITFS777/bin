3182990fbc7d15a108da06abe2486631787c4651
ITFS777 <1374924410@qq.com>
2020-01-21 19:24:03 +0800

    2020/01/21 19:24:03 by ITFS<Aryan.ITFS@Gmail.com>

--- Python/Base64EnDe.py
+++ Python/Base64EnDe.py
@@ -5,18 +5,30 @@ while True:
     flag = input('encode[e]/decode[d]/changeTable[c]\n>')
     if flag == 'e':
         string = input("string:")
-        print(str(base64.b64encode(string.encode('utf-8'))))
+        print(base64.b64encode(string.encode('utf-8')).decode())
         print()
     elif flag == 'd':
-        string = input("Base64code:")
-        print(base64.b64decode(string.translate(str.maketrans(newTable,oldTable))).decode())
+        string = input("Base64:")
+        result = base64.b64decode(string.translate(
+            str.maketrans(newTable, oldTable))).decode()
+        print("String:", result, "Hex:", sep='\n')
+        i = 0
+        for x in result:
+            i += 1
+            print("{:02X}".format(ord(x)), end=' ')
+            if(i % 16 == 0):
+                print()
+        print("\nOct:")
+        i = 0
+        for x in result:
+            i += 1
+            print("{:03d}".format(ord(x)), end=' ')
+            if(i % 16 == 0):
+                print()
         print()
     elif flag == 'c':
-        print("oldTable:",oldTable,sep='')
+        print("oldTable:", oldTable, sep='')
         newTable = input("newTable:")
         print("Table Changed")
     else:
         print("Illegal Input!")
-
-
-
